e236e9683431000108cfe56d7aeed8ccb2a41f1d
XIA FEI <xiafei571@gmail.com>
2023-01-21 14:09:49 +0900

    optimize space complication

--- bb/ValidPalindrome125.py
+++ bb/ValidPalindrome125.py
@@ -7,6 +7,19 @@ class Solution:
                 newStr += c.lower()
         return newStr == newStr[::-1]
 
+    def isPalindrome_v2(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while r > l and not self.isAlphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+
     def isAlphaNum(self, c):
         if (ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9')):
             return True
