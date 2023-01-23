0f766f0193befc477db242be9c4b316ce304cafb
XIA FEI <xiafei571@gmail.com>
2023-01-23 20:43:02 +0900

    iteration, stack

--- bb/BinaryTreePaths257.py
+++ bb/BinaryTreePaths257.py
@@ -21,6 +21,26 @@ class Solution:
         dfs(root, '')
         return res
 
-    
+    # Iterations
+     def binaryTreePaths2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+            return []
+        
+        paths = []
+        stack = [(root, str(root.val))]
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        
+        return paths    
 
     
