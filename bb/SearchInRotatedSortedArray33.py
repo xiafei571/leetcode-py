4193f3ddcd34ada66f2f35cfd16b6b544e685d21
XIA FEI <xiafei571@gmail.com>
2023-01-29 00:31:23 +0900

    分两种情况来处理，看左边排序好的，还是右边排序好的

---
+++ bb/SearchInRotatedSortedArray33.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            
+            # left sorted: left,3,4,mid...
+            if nums[l] <= nums[mid]:
+                if target < nums[l] or target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            #right sorted: mid, 5,6,7,right
+            else: 
+                if target > nums[r] or target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+                    
+        return -1
+                
