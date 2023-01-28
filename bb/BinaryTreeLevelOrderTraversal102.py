40a601fa648e87477d1622e3d28cbdd4196a49e8
XIA FEI <xiafei571@gmail.com>
2023-01-28 21:46:45 +0900

    collections.deque()  deque.popleft()

---
+++ bb/BinaryTreeLevelOrderTraversal102.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        queue = collections.deque([root] if root else [])
+        #bfs
+        while queue:
+            cur = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                cur.append(node.val)
+            res.append(cur)
+
+        return res
