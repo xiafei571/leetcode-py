ea238b630ae458facf1b27b54c6179872e0db035
XIA FEI <xiafei571@gmail.com>
2023-01-29 16:44:30 +0900

    用stack 解决

---
+++ bb/DecodeString394.py
@@ -0,0 +1,24 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        res = ''
+
+        for i in range(len(s)):
+            if s[i] != ']':
+                stack.append(s[i])
+            else:
+                substr = ''
+                while stack[-1] != '[':
+                    substr = stack.pop() + substr 
+                stack.pop()
+
+                num = ''
+                while stack and stack[-1].isdigit():
+                    num =  stack.pop() + num
+                
+                stack.append(substr * int(num))
+
+        return ''.join(stack)
+
+
+        
