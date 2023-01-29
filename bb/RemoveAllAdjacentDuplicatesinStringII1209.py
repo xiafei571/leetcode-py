e2753eb6cb4bd729a68d8b62e1f70960f99c2044
XIA FEI <xiafei571@gmail.com>
2023-01-29 13:48:35 +0900

    follow up >=

--- bb/RemoveAllAdjacentDuplicatesinStringII1209.py
+++ bb/RemoveAllAdjacentDuplicatesinStringII1209.py
@@ -15,3 +15,34 @@ class Solution:
             res += (ch * count)
 
         return res
+
+    def removeDuplicates2(self, s: str, k: int) -> str:
+        stack = [] #[char, int]
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+            elif stack and stack[-1][0] != c:
+                # different char
+                if stack[-1][1] >= k:
+                    stack.pop()
+                    if stack and stack[-1][0] == c:
+                        stack[-1][1] += 1
+                    else:
+                        stack.append([c, 1])
+            else:
+                stack.append([c, 1])
+
+        
+
+        res = ''
+        for ch, count in stack:
+            res += (ch * count)
+
+        return res
+
+if __name__ == "__main__":
+
+    obj = Solution()
+    print(obj.removeDuplicates('aaabbbbacc', k=3))
+
+    print(obj.removeDuplicates2('aabbbbacc', k=3))
