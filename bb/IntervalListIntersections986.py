e8f57fdcb9f5243de7fbabe9e5e6d0ad743a929f
XIA FEI <xiafei571@gmail.com>
2023-01-30 23:46:03 +0900

    比较每一个元素的末尾，哪个小就移动哪个list的指针

---
+++ bb/IntervalListIntersections986.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i, j = 0, 0
+        res = []
+        while i < len(firstList) and j < len(secondList):
+            if firstList[i][1] <  secondList[j][0]:
+                i+=1
+            elif firstList[i][0] > secondList[j][1]:
+                j+=1
+            else:
+                res.append([max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])])
+                if firstList[i][1] < secondList[j][1]:
+                    i+=1
+                else:
+                    j+=1
+        return res
+
+        
